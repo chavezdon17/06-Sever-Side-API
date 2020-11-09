ac94f3352c86bf4da3382d878e94e2daf0da5f1d
Donovan Chavez <chavezdon17@gmail.com>
2020-11-08 18:10:51 -0600

    added wind speed and uv index

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var inputValue = document.querySelector(".inputValue");
 var name = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
-var icon = document.querySelector("icon");
+var icn = document.querySelector(".icon");
+var hum = document.querySelector(".hum");
+var windSpeed = document.querySelector(".windSpeed");
+var uvel = document.querySelector(".uvIndex");
+var dayOne = document.querySelector(".fiveDayForcast");
 
 var $cityButtonEl = $("#city-button-element");
 var $recentSearchEl = $("#recent_searches");
@@ -28,27 +32,63 @@ button.addEventListener("click", function () {
     .then((response) => response.json())
     .then((data) => {
       //getting data
+      console.log(data);
       var nameValue = data["name"];
       var tempValue = data["main"]["temp"];
       var descValue = data["weather"][0]["description"];
-      var iconValue = data["icon"];
+      var iconValue = data.weather.icon;
+      var humidityValue = data["main"]["humidity"];
+      var windSpeedValue = data.wind.speed;
       lat = data.coord.lat;
       long = data.coord.lon;
-
+      console.log(humidityValue);
+      console.log(hum.innerHTML);
       temp.innerHTML = tempValue;
       desc.innerHTML = descValue;
-    })
+      icn.innerHTML = iconValue;
+      console.log(icn);
+      hum.innerHTML = humidityValue;
+      windSpeed.innerHTML = windSpeedValue;
+      console.log(windSpeedValue);
 
-    //alerting wrong input
-    .catch((err) => alert("Wrong city name!"));
-  fetch(
-    "https://api.openweathermap.org/data/2.5/onecall?" +
-      lat +
-      long +
-      "&appid=d99af31d18f58b92c977dbe59e43af60"
-  )
-    .then((response) => response.json())
-    .then((data) => {});
+      fetch(
+        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+          lat +
+          "&lon=" +
+          long +
+          "&appid=d99af31d18f58b92c977dbe59e43af60"
+      )
+        .then((response) => response.json())
+        .then((uvIndex) => {
+          console.log(uvIndex);
+          var uvIndexValue = uvIndex.value;
+          uvel.innerHTML = uvIndexValue;
+        });
+
+      fetch(
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+          inputValue.value +
+          "&units=imperial&appid=d99af31d18f58b92c977dbe59e43af60"
+      )
+        .then((response) => response.json())
+        .then((fiveDay) => {
+          console.log(fiveDay);
+          var fiveDayValue = fiveDay.list;
+
+          console.log(fiveDayValue[5].main);
+        });
+    });
+
+  //alerting wrong input
+  // .catch((err) => alert("Wrong city name!"));
+  //   fetch(
+  //     "https://api.openweathermap.org/data/2.5/onecall?" +
+  //       lat +
+  //       long +
+  //       "&appid=d99af31d18f58b92c977dbe59e43af60"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {});
 });
 
 //Loop through cityArray and create city buttons
